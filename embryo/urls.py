38d81f792fe4a8a0111c981f06ec53337731b638
unknown <Xavier@DEV003.3demotion.net>
2013-12-31 10:54:34 +0100

    cleaned some urls

--- embryo/urls.py
+++ embryo/urls.py
@@ -4,9 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'embryo.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+	# url(r'^$', 'embryo.views.home', name='home'),
+	url(r'^admin/', include(admin.site.urls))
 )
