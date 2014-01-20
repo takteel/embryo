3c73fb79f26d0c112f2177e41eef690154c86299
takteel <xavierguillaume@hotmail.fr>
2014-01-20 12:59:29 +0100

    Fixed 'index' url. Getting /game should now work

--- apps/game/urls.py
+++ apps/game/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('apps.game.views',
-	url(r'^/$', 'index', name='index'),
+	url(r'^$', 'index', name='index'),
 	url(r'^map/$', 'map', name='map'),
 	url(r'^colony/(?P<name>\w+)/$', 'colony', name='colony')
 )
