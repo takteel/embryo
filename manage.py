231eeacf0b5919ec0753b4e461e696337cbe1c24
takteel <guillax_3@hotmail.com>
2014-03-10 22:33:49 +0100

    FIXED default environment in manage.py
    default environment is now "development"

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-	os.environ.setdefault("EMBRYO_ENVIRONMENT", "production")
+	os.environ.setdefault("EMBRYO_ENVIRONMENT", "development")
 	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "embryo.settings.%s" % (os.environ["EMBRYO_ENVIRONMENT"],))
 
 	from django.core.management import execute_from_command_line
